79ffc71086a691e8df795fa1835b4b5a029c8269
ryo <ryo@indeed.com>
2024-07-29 17:30:14 +0900

    solution

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -1,24 +1,31 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   fetchSuggestion,
   selectError,
   selectLoading,
   // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
-} from './suggestion.slice';
-import './suggestion.css';
+  selectSuggestion,
+} from "./suggestion.slice";
+import "./suggestion.css";
 
 export default function Suggestion() {
   // Task 19: Call useSelector() with the selectSuggestion() selector
   // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
+  const suggestion = useSelector(selectSuggestion);
+  const { imageUrl, caption } = suggestion || {};
+
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    async function loadSuggestion() {
-      // Task 20: Dispatch the fetchSuggestion() action creator
-    }
+    // async function loadSuggestion() {
+    //   // Task 20: Dispatch the fetchSuggestion() action creator
+    // }
+    const loadSuggestion = async () => {
+      await dispatch(fetchSuggestion());
+    };
     loadSuggestion();
   }, [dispatch]);
 
@@ -31,8 +38,8 @@ export default function Suggestion() {
     // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
     render = (
       <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
+        <img alt={caption} src={imageUrl} />
+        <p>{caption}</p>
       </>
     );
   }
--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -1,28 +1,48 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchSuggestion =
-  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */);
+export const fetchSuggestion = createAsyncThunk(
+  "suggestion/fetchSuggestion", // Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion
+  async () => {
+    const response = await fetch("http://localhost:3004/api/suggestion");
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-  suggestion: '',
+  suggestion: "",
   loading: false,
-  error: true,
+  //error: true,
+  error: null,
 };
 
-const options = {
-  name: 'suggestion',
+const options = createSlice({
+  name: "suggestion",
   initialState,
   reducers: {},
-  extraReducers: {
-    /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+  extraReducers: (builder) => {
+    // Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call
+    builder
+      .addCase(fetchSuggestion.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchSuggestion.fulfilled, (state, action) => {
+        state.loading = false;
+        state.suggestion = action.payload;
+      })
+      .addCase(fetchSuggestion.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
-};
+});
 
-const suggestionSlice = createSlice(options);
-
-export default suggestionSlice.reducer;
+//export default suggestionSlice.reducer;
+export default options.reducer;
 
 // Task 17: Create a selector, called `selectSuggestion`, for the `suggestion` state variable and export it from the file
+export const selectSuggestion = (state) => state.suggestion.suggestion.data;
 
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
